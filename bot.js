4f11828c8237b99f8869127eb854115c7f7ebb41
pitardeveloper <37588350+pitardeveloper@users.noreply.github.com>
2018-09-26 15:06:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -728,6 +728,31 @@ message.react("??")
 });
 
 
+client.on('ready',async () => {
+  console.log(client.user.username);
+  try {
+    const config = {
+      name: "Rainbow", // اسم الرتبة
+      guildid: "467405825217134596", // اي دي السيرفر
+      sec: 2 // عدد الثواني
+    };
+    let guild = client.guilds.get(config.guildid);
+    let role = guild.roles.find(role => role.name === config.name);
+    let sec = config.sec * 1000;
+    if(!guild) return console.warn("Unkown guild.");
+    if(!role) return console.warn("Unkown role");
+    if(role.position >= guild.members.get(client.user.id).highestRole.position) return console.warn("**يجب ان تكون رتبة البوت فوق رتبة الرينبو**");
+    setInterval(() => {
+      role.edit({ 
+      color: "RANDOM"
+    });
+    }, sec);
+  } catch(e) {
+    console.error(e);
+  }
+}); 
+
+
 client.on("roleCreate",  rc => {
 const channel = rd.guild.channels.find("name", "console")
 if(channel) {
