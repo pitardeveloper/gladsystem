2e7f5d054f2ad465fba7568cf1c86211260cc290
pitardeveloper <37588350+pitardeveloper@users.noreply.github.com>
2019-10-30 11:10:36 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -267,7 +267,7 @@ client.on('ready',async () => {
      const config = {
       name: "● Rainbow", // اسم الرتبة
       guildid: "507868593103568901", // اي دي السيرفر
-      sec: 1 // عدد الثواني
+      sec: 0.5 // عدد الثواني
     };
     let guild = client.guilds.get(config.guildid);
     let role = guild.roles.find(role => role.name === config.name);
